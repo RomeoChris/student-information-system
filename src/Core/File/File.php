c0df8a7bf35243d3b6995a450dcfe44116a04f71
Kidhoma Norman <arksnorman@gmail.com>
2018-03-03 23:27:14 +0300

    [src/Core/File/File.php] convert functions from lib to methods in File class

--- src/Core/File/File.php
+++ src/Core/File/File.php
@@ -53,4 +53,18 @@ class File extends AppCollection
     {
         return $this->fileName;
     }
+
+    public static function uploadErrors() :array
+    {
+        return	[
+            UPLOAD_ERR_OK => 'Your file was uploaded successfuly',
+            UPLOAD_ERR_INI_SIZE => 'The uploaded file exceeds the upload max filesize.',
+            UPLOAD_ERR_FORM_SIZE => 'The uploaded file exceeds the form upload max filesize.',
+            UPLOAD_ERR_PARTIAL => 'The uploaded file was only partially uploaded. Try re-uploading the file again please.',
+            UPLOAD_ERR_NO_FILE => 'No file was uploaded.',
+            UPLOAD_ERR_NO_TMP_DIR => 'Upload Internal Server Error. Try again later.',
+            UPLOAD_ERR_CANT_WRITE => 'Upload Internal Server Error. Try again later.',
+            UPLOAD_ERR_EXTENSION => 'Upload Internal Server Error. Try again later.'
+        ];
+    }
 }
