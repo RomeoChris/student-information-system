989f27306e9fcc500976cb4ca9bd56e79c21d8e0
Kidhoma Norman <arksnorman@gmail.com>
2018-03-05 18:17:11 +0300

    [src/Conversions/TimeTableConversion.php] Add ApiColumns() method

--- src/Conversions/TimeTableConversion.php
+++ src/Conversions/TimeTableConversion.php
@@ -7,4 +7,23 @@ class TimeTableConversion implements IConversion
 {
     public function getIdField(): string { return 'id'; }
     public function getTableName(): string { return 'timetables'; }
+
+    public function getApiColumns() :array
+    {
+        return [
+            ['db' => 'description', 'dt' => 0],
+            ['db' => 'author', 'dt' => 1],
+            ['db' => 'date_created', 'dt' => 2,
+                'formatter' => function($d) { return date('jS M y', strtotime($d)); }
+            ],
+            ['db' => 'date_updated', 'dt' => 3,
+                'formatter' => function($d) { return !empty($d) ?date('jS M y', strtotime($d)) : ''; }
+            ],
+            ['db' => 'web_path', 'dt' => 4,
+                'formatter' => function($path){
+                    return "<a class='btn btn-primary' href='$path'><i class='fa fa-download'></i> Download</a>";
+                }
+            ]
+        ];
+    }
 }
