626eebf0a11c680309a04e3da58eb17be3314913
Kidhoma Norman <arksnorman@gmail.com>
2018-03-06 15:05:37 +0300

    [src/Conversions/ComplaintConversion.php] Add ApiColumns() method

--- src/Conversions/ComplaintConversion.php
+++ src/Conversions/ComplaintConversion.php
@@ -7,4 +7,23 @@ class ComplaintConversion implements IConversion
 {
     public function getIdField(): string { return 'id'; }
     public function getTableName(): string { return 'complaints'; }
+
+    public function getApiColumns() :array
+    {
+        return [
+            ['db' => 'title', 'dt' => 0],
+            ['db' => 'creator', 'dt' => 1],
+            ['db' => 'date_added', 'dt' => 2,
+                'formatter' => function($d) { return date('jS M y', strtotime($d)); }
+            ],
+            ['db' => 'date_updated', 'dt' => 3,
+                'formatter' => function($d) { return !empty($d) ? date('jS M y', strtotime($d)) : ''; }
+            ],
+            ['db' => 'id', 'dt' => 4,
+                'formatter' => function($id) {
+                    return "<a class='btn btn-info' href='/complaints/view/$id/'>View complaint</a>";
+                }
+            ]
+        ];
+    }
 }
