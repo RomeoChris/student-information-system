ed87fe354f8cb3b706c10bd379e1d0aca228c565
Kidhoma Norman <arksnorman@gmail.com>
2018-03-24 20:48:00 +0300

    [src/Conversions/ProfileConversion.php] Create API columns

--- src/Conversions/ProfileConversion.php
+++ src/Conversions/ProfileConversion.php
@@ -7,4 +7,24 @@ class ProfileConversion implements IConversion
 {
     public function getIdField() :string { return 'id'; }
     public function getTableName() :string { return 'profiles'; }
+
+    public function getApiColumns() :array
+    {
+        return [
+            ['db' => 'id', 'dt' => 0],
+            ['db' => 'username', 'dt' => 1],
+            ['db' => 'email', 'dt' => 2],
+            ['db' => 'first_name', 'dt' => 3],
+            ['db' => 'last_name', 'dt' => 4],
+            ['db' => 'role', 'dt' => 5],
+            [
+                'db' => 'id',
+                'dt' => 6,
+                'formatter' => function($id)
+                {
+                    return "<a class='btn btn-info' href='/users/view/$id/'>View Profile</a>";
+                }
+            ]
+        ];
+    }
 }
