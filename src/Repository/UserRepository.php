2f680c080712d61f01d5b703304ce7650a4fb829
Kidhoma Norman <arksnorman@gmail.com>
2018-04-02 23:56:54 +0300

    Add relation between user and course

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
@@ -12,6 +13,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
+
 class UserRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
