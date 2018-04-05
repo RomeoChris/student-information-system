487d1bf81924ba66184f7110a112a9b5741d9cf7
Kidhoma Norman <arksnorman@gmail.com>
2018-04-05 03:45:20 +0300

    Fix courses controller

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -5,35 +5,32 @@ namespace App\Controller;
 
 use App\Entity\Course;
 use App\Entity\Department;
+use App\Repository\DepartmentRepository;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CoursesController extends DefaultController
 {
     public function index() :Response
     {
-        if (!$this->getAuthenticator()->isLoggedIn())
-            return $this->redirectToRoute('index');
-
-        return $this->renderTemplate('courses/index.html.twig', [
-            'success' => $this->getSession()->flash('deleteSuccess'),
+        return $this->render('courses/index.html.twig', [
             'pageTitle' => 'All courses'
         ]);
     }
 
-    public function new() :Response
+    public function new(
+        Request $request,
+        DepartmentRepository $repository,
+        EntityManagerInterface $entityManager) :Response
     {
-        if (!$this->getAuthenticator()->isLoggedIn())
-            return $this->redirectToRoute('index');
-
-        $this->getAuthenticator()->requireAdmin();
-
-        $years = $this->getPost()->getInt('years');
+        $years = $request->request->getInt('years', 0);
         $errorList = [];
-        $courseName = $this->getPost()->get('courseName', '');
-        $departmentId = $this->getPost()->getInt('departmentId');
+        $courseName = $request->request->get('courseName', '');
+        $departmentId = $request->request->getInt('departmentId', 0);
 
-        if ($this->getRequest()->isMethod('post'))
+        if ($request->isMethod('post'))
         {
             if (empty(($years || $courseName || $departmentId)))
                 $errorList[] = 'All fields are required';
@@ -44,59 +41,54 @@ class CoursesController extends DefaultController
                 $course->setName($courseName);
                 $course->setYears($years);
                 $course->setDateCreated(new DateTime());
-                $course->setDepartmentId($departmentId);
+                $course->setDepartment($this->getDepartment($departmentId, $repository));
 
-                $this->entityManager->persist($course);
-                $this->entityManager->flush();
+                $entityManager->persist($course);
+                $entityManager->flush();
 
-                $this->getSession()->set('newCourseSuccess', 'Course has been successfully added');
+                $this->addFlash('success', 'Course has been successfully added');
                 return $this->redirectToRoute('newCourse');
             }
         }
 
-        return $this->renderTemplate('courses/new.html.twig', [
+        return $this->render('courses/new.html.twig', [
             'years' => $years,
             'errors' => $errorList,
-            'success' => $this->getSession()->flash('newCourseSuccess'),
             'pageTitle' => 'Add new course',
             'courseName' => $courseName,
-            'departments' => $this->getDepartmentRepository()->findAll(),
+            'departments' => $repository->findAll(),
         ]);
     }
 
     public function view(Course $course) :Response
     {
-        if (!$this->getAuthenticator()->isLoggedIn())
-            return $this->redirectToRoute('index');
-
-        return $this->renderTemplate('courses/view.html.twig', [
+        return $this->render('courses/view.html.twig', [
             'id' => $course->getId(),
             'years' => $course->getYears(),
             'pageTitle' => $course->getName(),
             'courseName' => $course->getName(),
-            'department' => $this->getDepartment($course->getDepartmentId())->getName()
+            'department' => $course->getDepartment()->getName()
         ]);
     }
 
-    public function edit(Course $course) :Response
+    public function edit(
+        Course $course,
+        Request $request,
+        DepartmentRepository $repository,
+        EntityManagerInterface $entityManager) :Response
     {
-        if (!$this->getAuthenticator()->isLoggedIn())
-            return $this->redirectToRoute('index');
-
-        $this->getAuthenticator()->requireAdmin();
-
         $errorList = [];
 
         $years = $course->getYears();
         $courseName = $course->getName();
-        $departmentId = $course->getDepartmentId();
-        $departmentName = $this->getDepartment($course->getDepartmentId())->getName();
+        $departmentId = $course->getDepartment()->getId();
+        $departmentName = $course->getDepartment()->getName();
 
-        if ($this->getRequest()->isMethod('post'))
+        if ($request->isMethod('post'))
         {
-            $years = $this->getPost()->get('years');
-            $courseName = $this->getPost()->get('courseName');
-            $departmentId = $this->getPost()->get('departmentId');
+            $years = $request->request->getInt('years');
+            $courseName = $request->request->get('courseName');
+            $departmentId = $request->request->getInt('departmentId');
 
             if (empty(($years || $courseName || $departmentId)))
                 $errorList[] = 'All fields are required';
@@ -105,48 +97,42 @@ class CoursesController extends DefaultController
             {
                 $course->setName($courseName);
                 $course->setYears($years);
-                $course->setDepartmentId($departmentId);
+                $course->setDepartment($this->getDepartment($departmentId, $repository));
                 $course->setDateModified(new DateTime());
 
-                $this->entityManager->persist($course);
-                $this->entityManager->flush();
+                $entityManager->persist($course);
+                $entityManager->flush();
 
-                $this->getSession()->set('editCourseSuccess', 'Course has been successfully updated');
+                $this->addFlash('success', 'Course has been successfully updated');
                 return $this->redirectToRoute('editCourse', ['id' => $course->getId()]);
             }
         }
 
-        return $this->renderTemplate('courses/edit.html.twig', [
+        return $this->render('courses/edit.html.twig', [
             'id' => $course->getId(),
             'years' => $years,
             'errors' => $errorList,
-            'success' => $this->getSession()->flash('editCourseSuccess'),
             'pageTitle' => 'Edit course',
             'courseName' => $courseName,
-            'departments' => $this->getDepartmentRepository()->findAll(),
+            'departments' => $repository->findAll(),
             'departmentId' => $departmentId,
             'departmentName' => $departmentName,
         ]);
     }
 
-    public function delete(Course $course) :Response
+    public function delete(Course $course, EntityManagerInterface $entityManager) :Response
     {
-        if (!$this->getAuthenticator()->isLoggedIn())
-            return $this->redirectToRoute('index');
+        $entityManager->remove($course);
+        $entityManager->flush();
 
-        $this->getAuthenticator()->requireAdmin();
-
-        $this->entityManager->remove($course);
-        $this->entityManager->flush();
-
-        $this->getSession()->set('deleteSuccess', 'Course deleted successfully');
+        $this->addFlash('success', 'Course deleted successfully');
         return $this->redirectToRoute('courses');
     }
 
-    private function getDepartment(int $id = 0) :Department
+    private function getDepartment(int $id = 0, DepartmentRepository $repository) :?Department
     {
         /* @var $department Department */
-        $department = $this->getDepartmentRepository()->find($id);
+        $department = $repository->find($id);
         return $department;
     }
 }
