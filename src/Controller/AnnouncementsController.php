69c3c037160bd34bb8e26442ffd65f3f3095dcfa
Kidhoma Norman <arksnorman@gmail.com>
2018-04-07 07:35:55 +0300

    Use symfony forms logic

--- src/Controller/AnnouncementsController.php
+++ src/Controller/AnnouncementsController.php
@@ -4,7 +4,8 @@ namespace App\Controller;
 
 
 use App\Entity\Announcement;
-use DateTime;
+use App\Form\AnnouncementType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,45 +13,33 @@ class AnnouncementsController extends DefaultController
 {
 	public function index() :Response
 	{
-		return $this->render('announcements/index.html.twig', [
-            'pageTitle' => 'All announcements'
-        ]);
+	    $params = ['pageTitle' => 'All announcements'];
+		return $this->render('announcements/index.html.twig', $params);
 	}
 
-	public function new(Request $request) :Response
+	public function new(Request $request, EntityManagerInterface $entityManager) :Response
 	{
-        $title = $request->request->get('title');
-        $author = $this->getUser()->getUsername();
-        $message = $request->request->get('message');
-        $errorList = [];
-
-		if ($request->isMethod('post'))
-		{
-			if (empty(($title || $message)))
-				$errorList[] = 'All fields are required';
-
-			if (empty($errorList))
-			{
-			    $announcement = new Announcement();
-			    $announcement->setTitle($title);
-			    $announcement->setAuthor($author);
-			    $announcement->setMessage($message);
-			    $announcement->setDateCreated(new DateTime());
-
-			    $this->getEntityManager()->persist($announcement);
-			    $this->getEntityManager()->flush();
-
-                $this->addFlash('success', 'Announcement added');
-                return $this->redirectToRoute('newAnnouncement');
-			}
-		}
-
-		return $this->render('announcements/new.html.twig', [
-            'title' => $title,
-            'errors' => $errorList,
-            'message' => $message,
-            'pageTitle' => 'Add new announcement'
-        ]);
+        $announcement = new Announcement;
+        $form = $this->createForm(AnnouncementType::class, $announcement);
+        $form->handleRequest($request);
+        
+        $params = [
+            'form' => $form->createView(),
+            'pageTitle' => 'New Announcement',
+            'formHeader' => 'New Announcement',
+        ];
+        
+        if (!$form->isSubmitted() || !$form->isValid())
+            return $this->render('announcements/new.html.twig', $params);
+        
+        $announcement->setAuthor($this->getUser()->getUsername());
+        $announcement->setDateCreated();
+        
+        $entityManager->persist($announcement);
+        $entityManager->flush();
+        
+        $this->addFlash('success', 'Announcement successfully posted');
+        return $this->redirectToRoute('newAnnouncement');
 	}
 
 	public function view(Announcement $announcement) :Response
@@ -58,45 +47,38 @@ class AnnouncementsController extends DefaultController
         return $this->render('announcements/view.html.twig', [
             'id' => $announcement->getId(),
             'title' => $announcement->getTitle(),
+            'author' => $announcement->getAuthor(),
             'message' => $announcement->getMessage(),
+            'dateTime' => $announcement->getDateCreated(),
             'pageTitle' => $announcement->getTitle(),
         ]);
 	}
 
-	public function edit(Announcement $announcement, Request $request) :Response
+	public function edit(
+	    Announcement $announcement,
+        Request $request,
+        EntityManagerInterface $entityManager) :Response
 	{
-        $title = $request->request->get('title', '');
-        $author = $this->getUser()->getUsername();
-        $message = $request->request->get('message', '');
-        $errorList = [];
-
-        if ($request->isMethod('post'))
-        {
-            if (empty(($title || $message)))
-                $errorList[] = 'All fields are required';
-
-            if (empty($errorList))
-            {
-                $announcement->setAuthor($author);
-                $announcement->setTitle($title);
-                $announcement->setMessage($message);
-                $announcement->setDateModified(new DateTime());
-
-                $this->getEntityManager()->persist($announcement);
-                $this->getEntityManager()->flush();
-
-                $this->addFlash('success', 'Announcement updated successfully');
-                return $this->redirectToRoute('editAnnouncement', ['id' => $announcement->getId()]);
-            }
-        }
-
-        return $this->render('announcements/edit.html.twig', [
-            'id' => $announcement->getId(),
-            'title' => $announcement->getTitle(),
-            'errors' => $errorList,
-            'message' => $announcement->getMessage(),
-            'pageTitle' => 'Update announcement ' . $announcement->getId(),
-        ]);
+        $form = $this->createForm(AnnouncementType::class, $announcement);
+        $form->handleRequest($request);
+        
+        $params = [
+            'form' => $form->createView(),
+            'pageTitle' => 'Update Announcement',
+            'formHeader' => 'Update Announcement',
+        ];
+        
+        if (!$form->isSubmitted() || !$form->isValid())
+            return $this->render('announcements/edit.html.twig', $params);
+        
+        $announcement->setAuthor($this->getUser()->getUsername());
+        $announcement->setDateModified();
+        
+        $entityManager->persist($announcement);
+        $entityManager->flush();
+        
+        $this->addFlash('success', 'Announcement updated successfully');
+        return $this->redirectToRoute('editAnnouncement', ['id' => $announcement->getId()]);
 	}
 
 	public function delete(Announcement $announcement) :Response
