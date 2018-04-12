987ebd898c0824f7fd86d22ece62ea95f3d66247
Kidhoma Norman <arksnorman@gmail.com>
2018-04-13 02:56:52 +0300

    Remove config class

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,38 +3,20 @@
 namespace App\Controller;
 
 
-use App\Core\DataTable\DataTable;
-use App\Core\Configuration\Configuration;
 use App\Entity\User;
 use App\Repository\AnnouncementRepository;
 use App\Repository\ComplaintRepository;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     protected static $ds = DIRECTORY_SEPARATOR;
 
-    protected function getConfiguration() :Configuration
-    {
-        return new Configuration($this->getRootDir());
-    }
- 
     protected function getRootDir() :string
     {
         return __DIR__ . self::$ds . '..' . self::$ds . '..' . self::$ds;
-    }
-    
-    protected function getDatabaseConfig() :ParameterBag
-    {
-        return new ParameterBag(self::getConfiguration()->getData()['database']);
-    }
-
-    protected function getDataTable() :DataTable
-    {
-        return new DataTable($this->getDatabaseConfig());
     }
 
     protected function getProfile() :User
